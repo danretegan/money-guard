1a801f507501a1d5ffc2d7239bb16aab9411a9d4
Dan Retegan <danretegan@yahoo.com>
2024-04-16 09:39:56 +0300

    Updates

---
+++ assets/LoadingScreenSharedLayoutPages-DOM0Ok5X.js
@@ -0,0 +1,1 @@
+import{j as a,D as e}from"./index-DUDc7bV7.js";const n="_loadingScreenSharedLayoutPages_r0jas_1",s={loadingScreenSharedLayoutPages:n},r=()=>a.jsx(a.Fragment,{children:a.jsx("div",{className:s.loadingScreenSharedLayoutPages,children:a.jsx("img",{src:e,alt:"logo",className:"animate__animated  animate__rotateOut animate__infinite animate__slow"})})});export{r as L};
